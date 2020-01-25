2f74bf44df89059158cb8a5bc3ac2bfd5c95f029
ElianMelo <elianmelomorais@gmail.com>
2020-01-24 21:45:37 -0300

    Estudo de rotas e diretivas

--- angularjs-w3s/config/app.js
+++ angularjs-w3s/config/app.js
@@ -1,1 +1,17 @@
-var app = angular.module('app', []);
+var app = angular.module("app", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "./routes/home.html"
+    })
+    .when("/testa", {
+        template : "<h1>Teste A</h1>"
+    })
+    .when("/testb", {
+        template : "<h1>Teste B</h1>" 
+    })
+    .when("/testc", {
+        template : "<h1>Teste C</h1>"
+    })
+});
--- angularjs-w3s/controllers/control.js
+++ angularjs-w3s/controllers/control.js
@@ -1,3 +1,16 @@
-app.controller('control', function($scope){
-    $scope.helloWorld = 'Hello World';
+app.controller("control", function($scope){
+    $scope.nomes = ["elian", "joao", "roberto", "ana", "maria"];
+
+    $scope.mensagem = "Titulo da Aplicacao";
+
+    $scope.alterar = function(){
+        $scope.mensagem = $scope.text;
+        return;
+    }
+
+    $scope.adicionar = function(){
+        $scope.nomes.push($scope.item);
+        $scope.item = "";
+    }
+
 });

9d83f913708f488f4fea9a33ac7da15e9b39a605
ElianMelo <elianmelomorais@gmail.com>
2020-08-06 22:58:28 -0300

    Realiza a listagem de professores

--- nextlevelweek2/web/src/components/TeacherItem/index.tsx
+++ nextlevelweek2/web/src/components/TeacherItem/index.tsx
@@ -4,31 +4,41 @@ import whastsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/41520689?s=460&u=9d0b79fcd5a239c97a1ff4c7951024f34294a66f&v=4" alt="Elian Melo" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Elian Melo</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p>
-                Entusiasta das melhores tecnologias de química avançada.
-            <br /><br />
-            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma de minhas explosões.
-        </p>
+            <p>{teacher.bio}</p>
             <footer>
                 <p>
                     Preço/hora
-                <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
 
-                <button type="button">
+                <a href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whastsappIcon} alt="Whatsapp" />
-                Entrar em contato
-            </button>
+                    Entrar em contato
+                </a>
             </footer>
         </article>
     );
